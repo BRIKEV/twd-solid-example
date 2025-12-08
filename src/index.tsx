bb604ce3b362c7f30bb3ec5c0b8bf4d1a3868f85
kevinccbsg <kevinccbsg@gmail.com>
2025-12-08 12:02:54 +0100

    first: commit with twd

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+/* @refresh reload */
+import { render } from 'solid-js/web';
+import 'solid-devtools';
+
+import App from './App';
+
+if (import.meta.env.DEV) {
+  const { initTWD } = await import('twd-js/bundled');
+  const tests = import.meta.glob("./**/*.twd.test.ts")
+  initTWD(tests);
+}
+
+const root = document.getElementById('root');
+
+if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
+  throw new Error(
+    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+  );
+}
+
+render(() => <App />, root!);
